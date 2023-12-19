79003e04e0d6fbdf25cc3c2b9925bd872cccdf0d
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-12-19 11:56:48 +0100

    Bumper dusseldorf-ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val dusseldorfKtorVersion = "4.1.3"
+val dusseldorfKtorVersion = "4.1.4"
 val ktorVersion = "2.3.7"
 val slf4jVersion = "2.0.9"
 val amazonawsVersion = "1.11.790"
