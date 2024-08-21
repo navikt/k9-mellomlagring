0451f9dbe550adcbfcdd5bb8a19dfd30de70ae27
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-08-21 12:19:42 +0200

    Overstyrer JacksonStatusPages for bedre feilmelding.

--- src/main/kotlin/no/nav/helse/K9Mellomlagring.kt
+++ src/main/kotlin/no/nav/helse/K9Mellomlagring.kt
@@ -1,16 +1,22 @@
 package no.nav.helse
 
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.JsonMappingException
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.metrics.micrometer.*
+import io.ktor.server.plugins.*
 import io.ktor.server.plugins.callid.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.routing.*
+import io.ktor.util.*
 import io.prometheus.client.hotspot.DefaultExports
 import no.nav.helse.dokument.DokumentService
 import no.nav.helse.dokument.VirusScanner
@@ -23,7 +29,6 @@ import no.nav.helse.dusseldorf.ktor.auth.AuthStatusPages
 import no.nav.helse.dusseldorf.ktor.auth.idToken
 import no.nav.helse.dusseldorf.ktor.core.*
 import no.nav.helse.dusseldorf.ktor.health.*
-import no.nav.helse.dusseldorf.ktor.jackson.JacksonStatusPages
 import no.nav.helse.dusseldorf.ktor.jackson.dusseldorfConfigured
 import no.nav.helse.dusseldorf.ktor.metrics.MetricsRoute
 import no.nav.helse.dusseldorf.ktor.metrics.init
@@ -32,6 +37,7 @@ import no.nav.security.token.support.v2.asIssuerProps
 import no.nav.security.token.support.v2.tokenValidationSupport
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.net.URI
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -178,5 +184,81 @@ class StorageHealthCheck(
             logger.error("Feil ved tilkobling mot storage.", cause)
             UnHealthy(name = name, result = cause.message ?: "Feil ved tilkobling mot storage.")
         }
+    }
+}
+
+private val logger: Logger = LoggerFactory.getLogger("no.nav.helse.dusseldorf.ktor.jackson.JacksonStatusPages")
+
+@OptIn(InternalAPI::class)
+fun StatusPagesConfig.JacksonStatusPages() {
+
+    exception<JsonMappingException> { call, cause ->
+        if (cause.cause is IllegalArgumentException) {
+            call.respondProblemDetails(
+                DefaultProblemDetails(
+                    title = "IllegalArgumentException",
+                    status = 400,
+                    detail = "${cause.cause as IllegalArgumentException} -> ${cause.path}"
+                ),
+                logger
+            )
+        } else {
+            val violations = mutableSetOf<Violation>()
+            cause.path.filter { it.fieldName != null }.forEach {
+                violations.add(
+                    Violation(
+                        parameterType = ParameterType.ENTITY,
+                        parameterName = it.fieldName,
+                        reason = "Må være satt.",
+                        invalidValue = null
+
+                    )
+                )
+            }
+
+            val problemDetails = ValidationProblemDetails(violations)
+
+            logger.debug("Feil ved mapping av JSON", cause)
+            call.respondProblemDetails(problemDetails, logger)
+        }
+    }
+
+    exception<JsonProcessingException> { call, cause ->
+
+        val problemDetails = DefaultProblemDetails(
+            title = "invalid-json-entity",
+            status = 400,
+            detail = "Request entityen inneholder ugyldig JSON."
+        )
+        logger.debug("Feil ved prosessering av JSON", cause)
+        call.respondProblemDetails(problemDetails, logger)
+    }
+
+    exception { call: ApplicationCall, cause: BadRequestException ->
+        val problemDetails = when (val rootCause = cause.rootCause) {
+            is MissingKotlinParameterException -> {
+                val parameter = rootCause.parameter
+                ValidationProblemDetails(
+                    setOf(
+                        Violation(
+                            parameterName = parameter.name ?: "ukjent",
+                            parameterType = ParameterType.ENTITY,
+                            reason = "Må være satt.",
+                            invalidValue = null
+                        )
+                    )
+                )
+            }
+            else -> {
+                DefaultProblemDetails(
+                    title = "invalid-request-parameters",
+                    status = 400,
+                    detail = cause.message ?: "Requesten inneholder ugyldige parametere",
+                    instance = URI(call.request.path())
+                )
+            }
+        }
+
+        call.respondProblemDetails(problemDetails, logger)
     }
 }
